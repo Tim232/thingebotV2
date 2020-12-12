1b9537eba9f8ec92596d7b5f940615ee9ad17a60
애브리띵 <66000432+OHvrything@users.noreply.github.com>
2020-12-12 16:41:36 +0900

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -33,7 +33,7 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print("준비 완료!")
-    messages = ["'?도움'을 입력해 띵이봇과 노는법을 알아보세요!","애브리띵#2227","이 메시지는 5초마다 변경됩니다!","https://thinge.teb.kro.kr","TEB 2.25"]
+    messages = ["'?도움'을 입력해 띵이봇과 노는법을 알아보세요!","애브리띵#2227","이 메시지는 5초마다 변경됩니다!","https://thinge.teb.kro.kr","TEB 2.26"]
     while True:
         await bot.change_presence(status=discord.Status.online, activity=discord.Game(name=messages[0]))
         messages.append(messages.pop(0))
@@ -405,8 +405,8 @@ async def myinfo(msg, *, user: discord.Member=None):
     if user is not None:
         try:    
             embed = discord.Embed(
-                    title=f"{user.name}#{user.discriminator}({user.mention})의 정보",
-                    description="다른 분의 정보를 보여드립니다...",
+                    title=f"{user.name}#{user.discriminator}의 정보",
+                    description=f"다른 분({user.mention})의 정보를 보여드립니다...",
                     color=RandomColor()
                 )
             embed.set_thumbnail(url=f"{user.avatar_url}")
@@ -425,8 +425,8 @@ async def myinfo(msg, *, user: discord.Member=None):
     else:
         try:    
             embed = discord.Embed(
-                    title=f"{msg.author.name}#{msg.author.discriminator}({msg.author.mention})의 정보",
-                    description="당신의 정보에요!",
+                    title=f"{msg.author.name}#{msg.author.discriminator}의 정보",
+                    description=f"당신({msg.author.mention})의 정보에요!",
                     color=RandomColor()
                 )
             embed.set_thumbnail(url=f"{msg.author.avatar_url}")
