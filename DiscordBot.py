8f5aac44ed176b4229851dab71be683a06b874d8
애브리띵 <66000432+OHvrything@users.noreply.github.com>
2020-12-22 13:55:29 +0900

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -561,4 +561,17 @@ async def on_member_remove(member):
     embed.set_thumbnail(url=member.avatar_url)
     await member.guild.system_channel.send(embed=embed)
 
+class TopGG(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.token = os.environ['dbltk'] # set this to your DBL token
+        self.dblpy = dbl.DBLClient(self.bot, self.token, autopost=True) # Autopost will post your guild count every 30 minutes
+
+    async def on_guild_post():
+        print("Server count posted successfully")
+
+def setup(bot):
+    bot.add_cog(TopGG(bot))
+                        
 bot.run(os.environ['token'])
